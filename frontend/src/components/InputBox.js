b9855869c0ddeaf012fbdaae6957b153e5b1d715
tanmay <abc.email.com>
2023-07-18 20:02:31 +0530

    css update for scroller for textarea

--- frontend/src/components/InputBox.js
+++ frontend/src/components/InputBox.js
@@ -16,13 +16,17 @@ const InputBox = ({ onSendMessage }) => {
 
   return (
     <div className="input-container">
-      <input
+            <textarea
         type="text"
         placeholder="Type your message..."
         value={message}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
         class="input-box"
+        autoFocus
+        rows={1} // Start with a single row
+        style={{ height: `${(message.split('\n').length + 1) * 30}px` }}
+    
       />
       <button onClick={() => onSendMessage(message)}  class="send-button">Ask</button>
 
